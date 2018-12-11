4591213e66b96378629de7df2d03e9674f540748
samuelbiros <samuel.biros@student.tuke.sk>
2018-12-11 10:50:48 +0100

    another tests added

--- app/src/test/java/link/standen/michael/slideshow/util/FileItemHelperTest.java
+++ app/src/test/java/link/standen/michael/slideshow/util/FileItemHelperTest.java
@@ -1,36 +1,65 @@
 package link.standen.michael.slideshow.util;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.net.Uri;
+import android.os.Environment;
 import android.test.suitebuilder.annotation.SmallTest;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.File;
 
+import link.standen.michael.slideshow.R;
 import link.standen.michael.slideshow.model.FileItem;
 
 import static org.junit.Assert.*;
+import static org.powermock.api.mockito.PowerMockito.when;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({Uri.class})
-public class FileItemHelperTest {
+@PrepareForTest({Uri.class, Environment.class})
+public class
+FileItemHelperTest {
 
     private FileItemHelper fileItemHelper;
 
     @Mock
     private Context mockContext;
 
-    @Test
-    @SmallTest
-    public void createFileItem() throws Exception {
+    @Mock
+    private Resources mockedResources;
+
+
+    @Rule
+    public TemporaryFolder storageDirectory = new TemporaryFolder();
+
+    private File nonExistentDirectory;
+    private File existentDirectory;
+
+    @Before
+    public void setup() {
         fileItemHelper = new FileItemHelper(mockContext);
 
+        nonExistentDirectory = Mockito.mock(File.class);
+        Mockito.when(nonExistentDirectory.exists()).thenReturn(false);
+
+        existentDirectory = storageDirectory.getRoot();
+
+        PowerMockito.mockStatic(Environment.class);
+    }
+
+    @Test
+    @SmallTest
+    public void createFileItem() {
         testFileCreate("testFileCreate", "testDir");
         testFileCreate("", "");
         testFileCreate(" ", " ");
@@ -51,20 +80,32 @@ public class FileItemHelperTest {
     @Test
     @SmallTest
     public void createGoHomeFileItem() {
+        // the external storage directory is available
+        Mockito.when(Environment.getExternalStorageDirectory()).thenReturn(existentDirectory);
+
+        String playItemString = "Stuck? Click to go Home";
+        when(mockContext.getResources()).thenReturn(mockedResources);
+        when(mockContext.getResources().getString(R.string.go_home_folder))
+                .thenReturn(playItemString);
+
+        FileItem realItem = fileItemHelper.createGoHomeFileItem();
+        assertEquals(playItemString, realItem.getName());
+        assertTrue(realItem.getIsSpecial());
+        assertEquals(existentDirectory.getAbsolutePath(), realItem.getPath());
+        assertEquals(true, realItem.getIsDirectory());
     }
 
     @Test
     @SmallTest
     public void createPlayFileItem() {
-    }
+        String playItemString = "Play Slideshow from here";
+        when(mockContext.getResources()).thenReturn(mockedResources);
+        when(mockContext.getResources().getString(R.string.play_folder))
+                .thenReturn(playItemString);
 
-    @Test
-    @SmallTest
-    public void isImage() {
-    }
-
-    @Test
-    @SmallTest
-    public void getImageMimeType() {
+        FileItem realItem = fileItemHelper.createPlayFileItem();
+        assertEquals(playItemString, realItem.getName());
+        assertTrue(realItem.getIsSpecial());
+        assertEquals(false, realItem.getIsDirectory());
     }
 }
